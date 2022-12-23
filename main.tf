26214decc240edf8fc3e9c496c75fd5ed17a3566
sumankumarghosh-ibm <64221745+sumankumarghosh-ibm@users.noreply.github.com>
2022-12-23 15:07:48 +0530

    test

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 resource "tls_private_key" "private_key" {
+    count=0
   algorithm = "RSA"
   rsa_bits = "2048"
 }
 resource "aws_key_pair" "key_pair" {
+    count =0
   key_name = var.name
-  public_key = tls_private_key.private_key.public_key_openssh
+  public_key = tls_private_key.private_key[0].public_key_openssh
   tags = {
     Name = var.name
   }
